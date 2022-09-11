23835c9c5610ec0a4c8fc944d9dc8af209c61d6a
FlorencioQuelca <florens.lonwe@gmail.com>
2022-09-10 21:52:45 -0400

    cv

---
+++ main.js
@@ -0,0 +1,5 @@
+const iconMenu = document.querySelector('#iconMenu');
+const menu = document.querySelector('#menu');
+iconMenu.addEventListener("click", function() {
+    menu.classList.toggle("menu_show")
+})
